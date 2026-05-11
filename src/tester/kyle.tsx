36e3f6a317974ce598003609899ed06c14929424
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2026-05-11 19:30:30 +0300

    dev

--- src/tester/kyle.tsx
+++ src/tester/kyle.tsx
@@ -1,3 +1,4 @@
+import { Button } from "../general/button";
 import { NeolitComponent, NeolitNode, state } from "../package/core";
 import { Outlet, RouteMap, Router, UrlParameters } from "../package/routing";
 export class KyleDaily extends NeolitComponent {
@@ -47,9 +48,14 @@ export class KyleBroflovski extends NeolitComponent<UrlParameters> {
   }
 
   render(): NeolitNode {
+    // alert("Kylelandın");
     return (
       <>
         {/* <div>{this.properties.childrenPath || "No path"}</div> */}
+        <h1>Kyle'landın</h1>
+        <Button onclick={() => this.router.navigate("daily")}>Daily Kyle</Button>
+        <Button onclick={() => this.router.navigate("elf-king")}>Elf King Kyle</Button>
+        <Button onclick={() => this.router.navigate("human-kite")}>Human Kite Kyle</Button>
         <Outlet router={this.router} />
       </>
     );
